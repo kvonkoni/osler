e1d9609cbd044f6336574a51116a4f001bfc35be
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-03-23 14:17:48 -0400

    Added criterion unit tests

---
+++ test/criterion_unit_test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os, sys, unittest
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+from pando.assertion import Assertion
+from pando.criterion import Criterion
+
+class TestCriterion(unittest.TestCase):
+
+    def test_assertion(self):
+        assertion = Assertion("This is an assertion", "This is the question that tests the assertion?", "These are instructions on how to test the assertion", 1.3, "Here are more details")
+        criterion = Criterion(assertion, True)
+        self.assertEqual(criterion.assertion, assertion)
+    
+    def test_truth_value(self):
+        assertion = Assertion("This is an assertion", "This is the question that tests the assertion?", "These are instructions on how to test the assertion", 1.3, "Here are more details")
+        criterion = Criterion(assertion, True)
+        self.assertEqual(criterion.truth_value, True)
+
+if __name__ == '__main__':
+    unittest.main()
