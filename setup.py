ef4cb6c97c4c18f69a7a8e2072b08863c4390381
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-04-13 10:04:58 -0400

    Updated setup, created readme

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 thelibFolder = os.path.dirname(os.path.realpath(__file__))
@@ -10,12 +10,25 @@ if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
         install_requires = f.read().splitlines()
 
-setup(name='Osler',
-      version='0.1',
-      description='Diagnostic Decision Tree Generator',
+readmePath = thelibFolder + '/README.md'
+long_description = ""
+if os.path.isfile(requirementPath):
+    with open(readmePath, "r") as fh:
+        long_description = fh.read()
+
+setup(name='osler',
+      version='0.1.0',
       author='Kier von Konigslow',
       author_email='kvonkonigslow@gmail.com',
-      url='kvonkonigslow.com/Osler',
-      packages=['osler'],
-      install_requires=install_requires
+      description='Diagnostic Decision Tree Generator',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://gitlab.com/kvonkoni/osler',
+      packages=find_packages(),
+      install_requires=install_requires,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
      )
