3438fea7ff633f088bc0176b91f52bb63bef9457
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-04-21 17:54:47 -0400

    Changed filenames to allow for manual tests

---
+++ test/manual_dignosis_test1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os,sys,math,csv
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+import numpy.matlib
+from osler.assertion import Assertion
+from osler.criterion import Criterion
+from osler.diagnosis import Diagnosis, diagnosable
+from osler.issue import Issue
+
+#Defining assertions
+
+assertionA = Assertion("assertion A", "is assertion A true?")
+assertionB = Assertion("assertion B", "is assertion B true?")
+assertionC = Assertion("assertion C", "is assertion C true?")
+assertionD = Assertion("assertion D", "is assertion D true?")
+assertionE = Assertion("assertion E", "is assertion E true?")
+assertionX = Assertion("assertion X", "is assertion X true?")
+assertionY = Assertion("assertion Y", "is assertion Y true?")
+
+#Defining criteria
+
+criterionA = Criterion(assertionA, True)
+criterionB = Criterion(assertionB, True)
+criterionC = Criterion(assertionC, True)
+criterionD = Criterion(assertionD, True)
+criterionE = Criterion(assertionE, True)
+
+criterionNA = Criterion(assertionA, False)
+criterionNB = Criterion(assertionB, False)
+criterionNC = Criterion(assertionC, False)
+criterionND = Criterion(assertionD, False)
+criterionNE = Criterion(assertionE, False)
+
+criterionX = Criterion(assertionX, True)
+criterionNY = Criterion(assertionY, False)
+
+#Defining diagnoses
+
+diagnosis1 = Diagnosis('Diagnosis 1', 'Diagnosis 1', 'Remedy 1', {criterionA, criterionNB, criterionC, criterionX}, 0.25)
+diagnosis2 = Diagnosis('Diagnosis 2', 'Diagnosis 2', 'Remedy 2', {criterionNA, criterionNC, criterionD, criterionNY, criterionX}, 0.15)
+diagnosis3 = Diagnosis('Diagnosis 3', 'Diagnosis 3', 'Remedy 3', {criterionNA, criterionC, criterionX}, 0.5)
+diagnosis4 = Diagnosis('Diagnosis 4', 'Diagnosis 4', 'Remedy 4', {criterionNA, criterionNC, criterionND, criterionE, criterionX}, 0.25)
+diagnosis5 = Diagnosis('Diagnosis 5', 'Diagnosis 5', 'Remedy 5', {criterionNA, criterionNC, criterionND, criterionNE, criterionX}, 0.025)
+
+#Same as 1
+diagnosis6 = Diagnosis('Diagnosis 6', 'Diagnosis 6', 'Remedy 6', {criterionA, criterionNB, criterionC, criterionX}, 0.25)
+
+#Testing diagnoses
+#Should pass
+diagnosable({diagnosis1, diagnosis2, diagnosis3, diagnosis5})
+
+#Should fail (1 and 6)
+diagnosable({diagnosis1, diagnosis2, diagnosis3, diagnosis5, diagnosis6})
