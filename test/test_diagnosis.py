6a487872dd8d5c2d43c15623696d75296d1849a7
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-04-21 18:07:02 -0400

    Created unit test for diagnosable function

--- test/test_diagnosis.py
+++ test/test_diagnosis.py
@@ -5,7 +5,7 @@ lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
 from osler.assertion import Assertion
 from osler.criterion import Criterion
-from osler.diagnosis import Diagnosis
+from osler.diagnosis import Diagnosis, diagnosable
 
 class TestDiagnosis(unittest.TestCase):
 
@@ -37,6 +37,18 @@ class TestDiagnosis(unittest.TestCase):
         diagnosis3 = Diagnosis('Diagnosis 1', 'Diagnosis description 1', 'Diagnosis remedy 1', {assertionA.true, assertionB.false, assertionC.false, assertionX.false}, 0.25)
         self.assertTrue(diagnosis1==diagnosis2)
         self.assertFalse(diagnosis1==diagnosis3)
+    
+    def test_diagnosable(self):
+        assertionA = Assertion("assertion A", "is assertion A true?")
+        assertionB = Assertion("assertion B", "is assertion B true?")
+        assertionC = Assertion("assertion C", "is assertion C true?")
+        assertionX = Assertion("assertion X", "is assertion X true?")
+        diagnosis1 = Diagnosis('Diagnosis 1', 'Diagnosis description 1', 'Diagnosis remedy 1', {assertionA.true, assertionB.false, assertionC.true, assertionX.false}, 0.25)
+        diagnosis2 = Diagnosis('Diagnosis 2', 'Diagnosis description 2', 'Diagnosis remedy 2', {assertionA.true, assertionB.false, assertionC.true, assertionX.false}, 0.5)
+        diagnosis3 = Diagnosis('Diagnosis 3', 'Diagnosis description 3', 'Diagnosis remedy 3', {assertionA.true, assertionB.false, assertionC.false, assertionX.false}, 0.25)
+        self.assertTrue(diagnosable({diagnosis1, diagnosis3}))
+        self.assertFalse(diagnosable({diagnosis1, diagnosis2}))
+
 
 if __name__ == '__main__':
     unittest.main()
