dc762d1ef8f567c42bf16d38497a7f679d2c8b0c
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-04-21 17:31:08 -0400

    Fixed various namespace bugs

--- example/login_example.py
+++ example/login_example.py
@@ -3,7 +3,7 @@ lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
 from osler.assertion import Assertion
 from osler.criterion import Criterion
-from osler.diagnosis import Diagnosis, CompareDiagnoses
+from osler.diagnosis import Diagnosis, compare_diagnoses
 from osler.issue import Issue
 from osler.engine import Matrix
 
@@ -37,19 +37,19 @@ no_internet_connection = Diagnosis("User does not have an internet connection", 
 no_attempt_reported = Diagnosis("No login attempt was recorded", "No login attempt was recorded since the user is using the global login", "Have the user use the site login page", {is_internet_connected_true, used_site_login_false}, 1000)
 incorrect_email = Diagnosis("User entered incorrect email address", "The user entered an incorrect email address", "Encourage the user to discover the correct alternate email address and then confirm.", {is_internet_connected_true, used_site_login_true, is_internet_connected_true, correct_email_false}, 2000)
 incorrect_password = Diagnosis("User entered incorrect password", "The user entered an incorrect password", "Send the user a password reset email link.", {is_internet_connected_true, used_site_login_true, correct_email_true, incorrect_password_true}, 100)
-invalid_password = Diagnosis("User entered incorrect password", "The user entered an incorrect password", "Send the user a password reset email link.", {is_internet_connected_true, used_site_login_true, correct_email_true, incorrect_password_true, able_to_set_password_false}, 100)
+invalid_password = Diagnosis("User entered incorrect password", "The user entered an incorrect password", "Send the user a password reset email link.", {is_internet_connected_true, used_site_login_true, correct_email_true, incorrect_password_true}, 100)
 email_filtered_as_spam = Diagnosis("User's invitation/password reset was filtered by their spam/junk folder", "The user's invitation/password reset was filtered by their spam/junk folder", "Put yourself as backup email, send new reset email, forward to user.", {is_internet_connected_true, email_in_spam_folder_true, received_password_reset_email_true, correct_email_true}, 10)
 email_blocked_by_firewall = Diagnosis("User email blocked by firewall", "The user's company firewall is blocking emails from Firmex", "Send the user's company's IT department the whitelisting instructions.", {is_internet_connected_true, email_in_spam_folder_false, received_password_reset_email_false, correct_email_true}, 10)
 
 #Defining an issue
 
-CompareDiagnoses(incorrect_password, email_filtered_as_spam)
+compare_diagnoses(incorrect_password, email_filtered_as_spam)
 
 issue = Issue("Can't login: Invalid Credentials", "The user cannot login to their account. They are seeing an error that says invalid credentials.", {no_internet_connection, no_attempt_reported, incorrect_email, incorrect_password, email_filtered_as_spam, email_blocked_by_firewall})
 
 #Building a test tree
 
 matrix = Matrix(issue)
-matrix.ConstructTree(True)
+matrix.construct_tree(True)
 
-matrix.node.To_png("login_example.png")
+matrix.node.to_png("login_example.png")
