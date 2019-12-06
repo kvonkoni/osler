b6003d8cef1cead092332d991ceb112452ca16a6
Kier von Konigslow <kvonkonigslow@gmail.com>
2019-12-06 14:11:44 -0500

    Fixed bug in tests

--- test/engine_tree_test1.py
+++ test/engine_tree_test1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import os,sys,math,csv
+import os,sys
+
 lib_path = os.path.abspath(os.path.join('..'))
 sys.path.append(lib_path)
 from osler.assertion import Assertion
@@ -11,13 +12,13 @@ from osler.engine import Matrix
 
 #Defining assertions
 
-assertionA = Assertion("assertion A", "is assertion A true?")
-assertionB = Assertion("assertion B", "is assertion B true?")
-assertionC = Assertion("assertion C", "is assertion C true?")
-assertionD = Assertion("assertion D", "is assertion D true?")
-assertionE = Assertion("assertion E", "is assertion E true?")
-assertionX = Assertion("assertion X", "is assertion X true?")
-assertionY = Assertion("assertion Y", "is assertion Y true?")
+assertionA = Assertion("assertion A")
+assertionB = Assertion("assertion B")
+assertionC = Assertion("assertion C")
+assertionD = Assertion("assertion D")
+assertionE = Assertion("assertion E")
+assertionX = Assertion("assertion X")
+assertionY = Assertion("assertion Y")
 
 #Defining criteria
 
@@ -38,17 +39,17 @@ criterionY = Criterion(assertionY, False)
 
 #Defining diagnoses
 
-diagnosis1 = Diagnosis('Diagnosis 1', 'Diagnosis 1', 'Remedy 1', {criterionA, criterionNB, criterionC, criterionX}, 0.25)
-diagnosis2 = Diagnosis('Diagnosis 2', 'Diagnosis 2', 'Remedy 2', {criterionNA, criterionNC, criterionD, criterionY, criterionX}, 0.15)
-diagnosis3 = Diagnosis('Diagnosis 3', 'Diagnosis 3', 'Remedy 3', {criterionNA, criterionC, criterionX}, 0.5)
-diagnosis4 = Diagnosis('Diagnosis 4', 'Diagnosis 4', 'Remedy 4', {criterionNA, criterionNC, criterionND, criterionE, criterionX}, 0.25)
-diagnosis5 = Diagnosis('Diagnosis 5', 'Diagnosis 5', 'Remedy 5', {criterionNA, criterionNC, criterionND, criterionNE, criterionX}, 0.025)
+diagnosis1 = Diagnosis('Diagnosis 1', {criterionA, criterionNB, criterionC, criterionX}, 0.25)
+diagnosis2 = Diagnosis('Diagnosis 2', {criterionNA, criterionNC, criterionD, criterionY, criterionX}, 0.15)
+diagnosis3 = Diagnosis('Diagnosis 3', {criterionNA, criterionC, criterionX}, 0.5)
+diagnosis4 = Diagnosis('Diagnosis 4', {criterionNA, criterionNC, criterionND, criterionE, criterionX}, 0.25)
+diagnosis5 = Diagnosis('Diagnosis 5', {criterionNA, criterionNC, criterionND, criterionNE, criterionX}, 0.025)
 
 #Defining an issue
 
-issue1 = Issue('Issue I', 'Issue I', {diagnosis1, diagnosis2, diagnosis3, diagnosis4, diagnosis5})
-issue2 = Issue('Issue I', 'Issue I', {diagnosis1, diagnosis2, diagnosis3, diagnosis4, diagnosis5})
-issue3 = Issue('Issue II', 'Issue II', {diagnosis1, diagnosis2, diagnosis3, diagnosis4})
+issue1 = Issue('Issue I', {diagnosis1, diagnosis2, diagnosis3, diagnosis4, diagnosis5})
+issue2 = Issue('Issue I', {diagnosis1, diagnosis2, diagnosis3, diagnosis4, diagnosis5})
+issue3 = Issue('Issue II', {diagnosis1, diagnosis2, diagnosis3, diagnosis4})
 
 #Building a test tree
 
@@ -68,4 +69,3 @@ print(matrix1.node == matrix2.node)
 print(matrix1.node == matrix3.node)
 
 matrix1.node.to_png("engine_test1.png")
-#matrix1.node.to_svg("engine_test1.svg")
